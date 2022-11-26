b2f9196e4620c0e7396e25e6d922dea01ad7ec3e
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-11-26 23:49:46 +0900

    모달창 버튼 위치 수정

--- user-register/src/components/InvalidModal.js
+++ user-register/src/components/InvalidModal.js
@@ -7,10 +7,12 @@ const InvalidModal = () => {
     <div className={stlyes.container_modal}>
       <h1>Invalid input</h1>
       <Card className={stlyes.box_modal}>
-        <p>Please enter a valid age({">"}0)</p>
-        <button type="button" className={stlyes.btn_okay}>
-          Okay
-        </button>
+        <div className={stlyes.container_alert}>
+          <p>Please enter a valid age({">"}0)</p>
+          <button type="button" className={stlyes.btn_okay}>
+            Okay
+          </button>
+        </div>
       </Card>
     </div>
   );
