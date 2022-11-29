d8f0ec813ab1b873d9ebc081d35cb0479728bbbf
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-11-29 23:20:53 +0900

    유효성 검사 및 에러메시지 기능 구현

--- user-register/src/components/InvalidModal.js
+++ user-register/src/components/InvalidModal.js
@@ -3,13 +3,12 @@ import Card from "./UI/Card";
 import stlyes from "./InvalidModal.module.css";
 
 const InvalidModal = (props) => {
-  console.log(props.visible);
   return (
     <div className={stlyes.container_modal}>
       <h1>Invalid input</h1>
       <Card className={stlyes.box_modal}>
         <div className={`${stlyes.container_alert} ${props.visible}`}>
-          <p>Please enter a valid age({">"}0)</p>
+          <p>{props.mesage}</p>
           <button type="button" className={stlyes.btn_okay}>
             Okay
           </button>
--- user-register/src/components/UserForm.js
+++ user-register/src/components/UserForm.js
@@ -4,19 +4,24 @@ import InvalidModal from "./InvalidModal";
 import styles from "./UserForm.module.css";
 
 const UserForm = () => {
-  const [visible, setvisible] = useState("");
+  const [isValid, setIsValid] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [mesage, setMesage] = useState("");
+
+  const checkInputHandler = (event) => {
+    setIsValid(event.target.value);
+  };
 
   const addSubmitevent = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
-    if (event.target.value === undefined) {
-      console.log("err1");
+    console.log(isValid);
+    if (isValid === "") {
+      setVisible(true);
+      setMesage("Please enter a valid name and age (none-empty values)");
     }
-  };
-
-  const checkinputHandler = (event) => {
-    if (+event.target.value < 0) {
-      visible = "visible";
+    if (+isValid < 0) {
+      setVisible(true);
+      setMesage("Please enter a valid age(>0)");
     }
   };
 
@@ -25,13 +30,13 @@ const UserForm = () => {
       <form onSubmit={addSubmitevent}>
         <Card className={styles.user_form}>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" />
+          <input type="text" name="username" onChange={checkInputHandler} />
           <label htmlFor="age">Age(Years)</label>
-          <input type="number" name="age" onChange={checkinputHandler} />
+          <input type="number" name="age" onChange={checkInputHandler} />
           <button className={styles.btn_add_user}>Add User</button>
         </Card>
       </form>
-      <InvalidModal visible={visible} />
+      {visible ? <InvalidModal mesage={mesage} /> : null}
     </div>
   );
 };
