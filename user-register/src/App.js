5896f1ca204c404c4b60c8ccf1030eb7004b262b
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-12-02 20:46:08 +0900

    Error Modal 기능 추가

--- user-register/src/App.js
+++ user-register/src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/User/AddUser";
+import UserList from "./components/User/UserList";
 import "./style/reset.css";
 
 const App = () => {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 };
--- user-register/src/components/User/AddUser.js
+++ user-register/src/components/User/AddUser.js
@@ -1,44 +1,77 @@
 import React, { useState } from "react";
-import Card from "./UI/Card";
-import InvalidModal from "./InvalidModal";
-import styles from "./UserForm.module.css";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import styles from "./AddUser.module.css";
 
-const AddUser = () => {
-  const [isValid, setIsValid] = useState("");
-  const [visible, setVisible] = useState(false);
-  const [mesage, setMesage] = useState("");
-
-  const checkInputHandler = (event) => {
-    setIsValid(event.target.value);
-  };
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(isValid);
-    if (isValid === "") {
-      setVisible(true);
-      setMesage("Please enter a valid name and age (none-empty values)");
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (none-empty values)",
+      });
+      return;
     }
-    if (+isValid < 0) {
-      setVisible(true);
-      setMesage("Please enter a valid age(>0)");
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
     }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
   };
 
   return (
     <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={styles.user_form}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" onChange={checkInputHandler} />
+          <input
+            type="text"
+            name="username"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          />
           <label htmlFor="age">Age(Years)</label>
-          <input type="number" name="age" onChange={checkInputHandler} />
-          <button type="submit" className={styles.btn_add_user}>
+          <input
+            type="number"
+            name="age"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type={"submit"} className={styles.btn_add_user}>
             Add User
-          </button>
+          </Button>
         </form>
       </Card>
-      {visible ? <InvalidModal mesage={mesage} /> : null}
     </div>
   );
 };
