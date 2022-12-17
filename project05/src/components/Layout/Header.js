7a07a8467e4e7aefd3656840213979f553fc29bd
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-12-17 16:18:59 +0900

    기능: Cart 모달창 기능 추가

--- project05/src/components/Layout/Header.js
+++ project05/src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import styles from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpeg";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShownCart} />
       </header>
       <div className={styles["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food"></img>
